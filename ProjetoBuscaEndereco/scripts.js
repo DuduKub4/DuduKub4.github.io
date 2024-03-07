48dd006097f53c69022435d282b1d4c904c038a4
DuduKub4 <eduardo.kuba17@gmail.com>
2024-03-07 13:19:23 -0300

    btf

--- ProjetoBuscaEndereco/scripts.js
+++ ProjetoBuscaEndereco/scripts.js
@@ -1,15 +1,19 @@
 $(document).ready(function(){
     
+    $('#cep').on('input', function() {
+        this.value = this.value.replace(/[^0-9]/g, '');
+    });
+
     $('body').on('click','#consultar', function(){
         consultaCep();
-    })
+    });
+
     function consultaCep(){
         const cep = $('#cep').val();
         if(cep.length !== 8){
               alert('Digite o CEP corretamente !')
               return false; 
         }
-
 
         $.ajax({
             url: 'https://viacep.com.br/ws/'+cep+'/json/',
@@ -18,8 +22,6 @@ $(document).ready(function(){
             success: function(data){
                 console.log(data);
             }
-        })
-
-
+        });
     }
-})
+});
