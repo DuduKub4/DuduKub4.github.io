023344b9f9bb11a471c725c869289024db853c06
DuduKub4 <105323622+DuduKub4@users.noreply.github.com>
2024-04-04 20:02:33 -0300

    Delete DataExata directory

--- DataExata/public/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    axios.get('/datetime')
-        .then(function (response) {
-            document.getElementById('datetime').textContent = response.data.date;
-        })
-        .catch(function (error) {
-            console.error('Erro ao obter data e hora:', error);
-        });
-});
