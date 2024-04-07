ac4917a944a917300a60fb55b9d659b5807bff05
DuduKub4 <eduardo.kuba17@gmail.com>
2024-04-07 15:40:02 -0300

    new

--- produtosts/src/server.ts
+++ produtosts/src/server.ts
@@ -1,40 +1,60 @@
+import fs from 'fs';
 import { PrismaClient } from '@prisma/client';
 import fastify from 'fastify';
 import { z } from 'zod';
 
 const prisma = new PrismaClient();
-const server = fastify();
+const app = fastify();
 
 // Defina o esquema de validação usando Zod
 const produtoSchema = z.object({
-  id: z.number(),
+  id: z.string(),
   nome: z.string(),
-  img: z.string(),
+  img: z.string().url(), // Validar que 'img' é uma URL
   descricao: z.string(),
-  createdAt: z.string(),
 });
 
-async function main() {
-  const produtos = await prisma.produto.findMany();
-  
-  // Validar os produtos recuperados do banco de dados
-  produtos.forEach(produto => {
-    if (typeof produto === 'object' && produto !== null) {
-      try {
-        produtoSchema.parse(produto);
-        console.log('Produto válido:', produto.nome);
-      } catch (error) {
-        console.error('Produto inválido:', produto.nome, (error as Error).message);
-      }
-    }
-  });
-}
+app.get('/produtos', async (request, reply) => {
+  try {
+    const produtos = await prisma.produtos.findMany({
+      select: {
+        id: true,
+        nome: true,
+        img: true,
+        descricao: true,
+      },
+    });
+    reply.send(produtos);
+  } catch (error) {
+    reply.status(500).send('Erro ao buscar produtos');
+  }
+});
 
-server.listen({
-  host: '0.0.0.0',
-  port: process.env.PORT ? Number(process.env.PORT) : 3333
-}).then(() => {
-  console.log('HTTP Server Running');
-}).catch(error => {
-  console.error('Error starting server:', error);
+app.post('/produtos', async (request, reply) => {
+  try {
+    const body = request.body;
+    // Validar o corpo da requisição usando o esquema Zod
+    const produto = produtoSchema.parse(body);
+
+    // Criar o produto no banco de dados
+    const novoProduto = await prisma.produtos.create({
+      data: produto,
+    });
+
+    reply.status(201).send(novoProduto);
+  } catch (error) {
+    console.error('Erro ao criar produto:', error);
+    reply.status(400).send('Erro ao criar produto');
+  }
+});
+
+const PORT = process.env.PORT || 5555;
+const HOST = process.env.HOST || 'localhost';
+
+app.listen(PORT, HOST, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Servidor rodando em ${address}`);
 });
